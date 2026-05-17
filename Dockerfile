1267fe325fb38eb048cb76d7bd42d15c5c198cb0
VladVes <vccs@inbox.ru>
2026-05-17 14:46:50 +0700

    update app dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 #Dockerfile
-FROM golang:tip-bookworm
+FROM golang:1.26-alpine AS builder
 WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
 COPY . .
-RUN go mod tidy
-CMD ["go", "run", "./cmd/main.go"]
-EXPOSE 8080
+RUN CGO_ENABLED=0 GOOS=linux go build -o main ./cmd
+
+FROM alpine:3.21
+WORKDIR /app
+COPY --from=builder /app/main .
+EXPOSE 8080 
+CMD ["./main"]
--- internal/config/app.go
+++ internal/config/app.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -8,19 +9,33 @@ import (
 )
 
 const (
-	defaultAppPort  = "8080"
+	prod            = "production"
+	defaultAppPort  = "3000"
 	defaultLogLevel = "info"
+	fallbackDsn     = "host=localhost user=postgres password=mysecretpassword dbname=postgres port=5432 sslmode=disable"
 )
 
 func init() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("error loading .env file")
+	if env := os.Getenv("ENV"); env != prod {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("error loading .env file")
+		}
 	}
 }
 
 type AppConfig struct {
 	Port     string
 	LogLevel string
+	Dsn      string
+}
+
+func buildDsn() string {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, name, port) // TODO: check sslmode for prod
 }
 
 func GetAppConfig() AppConfig {
@@ -37,5 +52,6 @@ func GetAppConfig() AppConfig {
 	return AppConfig{
 		Port:     port,
 		LogLevel: logLevel,
+		Dsn:      buildDsn(),
 	}
 }
